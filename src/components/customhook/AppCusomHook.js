80f91a35c7783df131001752d24edf6d4e054e16
leanhnhan147 <anhnhan9a3@gmail.com>
2023-09-04 23:27:19 +0700

    add useLinkNewTab useHover use custom hook

--- src/components/customhook/AppCusomHook.js
+++ src/components/customhook/AppCusomHook.js
@@ -2,21 +2,22 @@ import React from "react";
 import Dropdown from "./Dropdown ";
 import SidebarMenu from "./SideBarMenu";
 import useClickOutSide from "./useClickOutSide";
+import Blog from "./Blog";
 
 function AppCustomHook() {
   const { show, setShow, nodeRef } = useClickOutSide("span");
   return (
     <div>
-      <span
+      <Blog></Blog>
+      {/* <span
         onClick={() => setShow(true)}
         className="inline-block m-3 p-3 rounded-lg text-white bg-green-400 cursor-pointer"
       >
         Show menu
       </span>
       <SidebarMenu show={show} ref={nodeRef}></SidebarMenu>
-      <Dropdown></Dropdown>
+      <Dropdown></Dropdown> */}
     </div>
   );
 }
-
 export default AppCustomHook;
